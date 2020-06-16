12340201e6c0125027d2679d12a3a21fc842a667
senera-madushan <it16235298@my.sliit.lk>
2020-06-16 12:03:10 +0530

    Initial commit

---
+++ order-service/src/main/java/com/example/orderservice/OrderController.java
@@ -0,0 +1,45 @@
+package com.example.orderservice;
+
+import com.example.orderservice.model.Order;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class OrderController {
+
+    private final List<Order> orders = Arrays.asList(
+            new Order(1, 1, "Product A"),
+            new Order(2, 1, "Product B"),
+            new Order(3, 2, "Product C"),
+            new Order(4, 1, "Product D"),
+            new Order(5, 2, "Product E"));
+
+//    @GetMapping
+//    public List<Order> getAllOrders() {
+//        return orders;
+//    }
+    @GetMapping
+    public List<Order> getAllOrders(@RequestParam(required = false)Integer customerId) {
+        if(customerId!=null)
+            return orders.stream().filter(order -> customerId.equals(order.getCustomerId())).collect(Collectors.toList());
+        return orders;
+    }
+
+    @GetMapping("/{id}")
+    public Order getOrderById(@PathVariable int id) {
+        return orders.stream()
+                .filter(order -> order.getId() == id)
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+
+
+
+}
